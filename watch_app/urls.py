791c98ba862ef0cc4489b133dc744522fd35e8e9
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-19 16:05:16 +0300

    add login and profile edit url path

--- watch_app/urls.py
+++ watch_app/urls.py
@@ -3,10 +3,13 @@ from django.urls import path, include
 
 # local imports
 from .views import *
+from watch_app import views
 
 app_name = 'watch'
 urlpatterns = [
     path ('', HomeView.as_view(), name='home'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('edit_profile/<int:pk>/', views.edit_user, name='edit_profile'),
 ]
