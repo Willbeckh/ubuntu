499ce8ee9b0522cdf20c9eed2130543f79e1149e
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-19 18:03:17 +0300

    register user profile model to admin

--- watch_app/admin.py
+++ watch_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from watch_app.models import UserProfile
+
 
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['user']}),
+        ('Bio', {'fields': ['bio']}),
+        ('Phone', {'fields': ['phone']}),
+        ('Block', {'fields': ['block']}),
+        # ('Neighborhood', {'fields': ['neighborhood']}),
+        ('Photo', {'fields': ['photo']}),
+    ]
+    
+admin.site.register(UserProfile, ProfileAdmin)
