a1ff0a6c754022204fb79b93ae6e298ef34e672f
bruno <brunolalpachino@gmail.com>
2022-06-19 18:07:57 +0300

    changed the bio field

--- hood/models.py
+++ hood/models.py
@@ -5,4 +5,10 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='profile', default="avatar.png")
+    bio = models.TextField(null=True, blank=True)
+
+
+
+    def __str__(self):
+        self.user.username
     
