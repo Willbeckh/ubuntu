03632f3eb7846828c14bf0e1f4828716cb9e67ee
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-19 22:07:43 +0300

    update user forms

--- watch_app/forms.py
+++ watch_app/forms.py
@@ -7,7 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', ]
+        fields = ['first_name', 'last_name', 'email',]
 
         widgets = {
             'first_name' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name..'}),
