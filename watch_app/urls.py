c56b3232f64fc2b85b48f244a5de78c96202021c
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-19 14:55:11 +0300

    add login url path

--- watch_app/urls.py
+++ watch_app/urls.py
@@ -7,4 +7,6 @@ from .views import *
 app_name = 'watch'
 urlpatterns = [
     path ('', HomeView.as_view(), name='home'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
