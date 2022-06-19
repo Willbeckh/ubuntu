2503df7fadbdc554f2a33095152e0a0f1a1fc2a4
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-19 14:12:37 +0300

    add bootstrap & fontawes to apps list

--- guard_project/settings.py
+++ guard_project/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'watch_app'
+    # user custop app && third party
+    'watch_app',
+    # bs4
+    'bootstrap4',
+    # fontawesome
+    'fontawesomefree'
 ]
 
 MIDDLEWARE = [
