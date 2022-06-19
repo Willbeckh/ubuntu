64345f362a8dac376bf1e02750471d0207ca3f00
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-19 18:04:05 +0300

    update forms.py user form widgets

--- watch_app/forms.py
+++ watch_app/forms.py
@@ -18,9 +18,11 @@ class UserForm(forms.ModelForm):
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
         
         widgets = {
+            'first_name' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name....'}),
+            'last_name' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name..'}),
             'username' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'Username..'}),
             'email' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email...'}),
         }
