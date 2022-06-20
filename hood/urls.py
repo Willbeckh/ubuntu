3cfd6c4c17626a30a9aeb1b8ff0e51af4d5b39a9
bruno <brunolalpachino@gmail.com>
2022-06-21 00:07:35 +0300

    created the route for updating profile

--- hood/urls.py
+++ hood/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('register/', views.Register , name="register"),
     path('logout/', views.LogoutUser , name="logout"),
     path('profile/', views.profile , name="profile"),
+    path('profile/update/', views.Update_Profile , name="update_profile"),
+
     path('business/', views.Businesses , name="business"),
 
     path('profile/update/<int:pk>', views.edit_user, name='update'),
