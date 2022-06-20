3a2eb515a471c72dc577e88bee61de8b3bd02ef5
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-20 23:07:08 +0300

    ft: add bg color randomize for divs

--- static/script.js
+++ static/script.js
@@ -1,7 +1,6 @@
 const choice = document.querySelectorAll(".nav-btn");
 choice.forEach(function (choice) {
   choice.addEventListener("click", function (e) {
-    console.log("clicked");
     e.preventDefault();
     const choice = e.target.innerText;
     const business = document.querySelector(".business-section");
@@ -21,4 +20,17 @@ choice.forEach(function (choice) {
       facility.classList.add("d-none");
     }
   });
+});
+
+// randomize bg-color on facility divs
+$("document").ready(function () {
+  console.log("page loaded");
+  const cards = document.querySelectorAll(".facility-card");
+  const colors = ["#b5e48c", "#f1faee", "#F6F3FC", "#E8F2F8", "#E1FDFB"];
+  let new_color = colors[Math.floor(Math.random() * colors.length)];
+
+  cards.forEach(function (card) {
+    $(card).css("background-color", new_color);
+  });
+  // $(cards).css("background-color", new_color);
 });
