41e8cd2cf8e1d4d75b4e4e1a1b0a58180b5fb6f9
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-20 23:11:32 +0300

    create Posts models

--- watch_app/models.py
+++ watch_app/models.py
@@ -1,3 +1,4 @@
+from operator import mod
 from django.db.models.signals import post_save
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
@@ -49,7 +50,8 @@ class Neighborhood(models.Model):
     facilities = models.ManyToManyField(
         'Facility', blank=True, related_name='neighborhood')
     business = models.ManyToManyField(
-        'Business', blank=True, related_name='business')
+        'Business', blank=True, related_name='neighborhood')
+    posts = models.ManyToManyField('Post', blank=True, related_name='neighborrhood')
 
     def __str__(self):
         return self.name
@@ -103,3 +105,16 @@ class Business(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
+    title = models.CharField(max_length=100, blank=True, null=True, default='')
+    body = models.TextField(blank=True, null=True, default='')
+    picture = models.ImageField(upload_to='post_images', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    
