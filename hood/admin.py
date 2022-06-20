a9ff69686dd97910334a6789ac7d681db0d1f590
bruno <brunolalpachino@gmail.com>
2022-06-20 14:52:02 +0300

    regsitered the models

--- hood/admin.py
+++ hood/admin.py
@@ -3,3 +3,8 @@ from hood.models import *
 # Register your models here.
 
 admin.site.register(Profile)
+admin.site.register(Location)
+admin.site.register(NeighbourHood)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(Contact)
