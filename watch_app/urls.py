dbb9b40b23e4232a5ec02f3282bbc40f6f583544
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-21 16:24:22 +0300

    ft; add new post template route

--- watch_app/urls.py
+++ watch_app/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path, include
 
-
 # local imports
 from .views import *
 from watch_app import views
 
 app_name = 'watch'
 urlpatterns = [
-    path ('', HomeView.as_view(), name='home'),
+    path('', HomeView.as_view(), name='home'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/<int:pk>/update/', views.edit_user, name='edit_profile'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('post/', PostView.as_view(), name='post'),
+    # path('facility/', FacilityView.as_view(), name='facility')
 ]
