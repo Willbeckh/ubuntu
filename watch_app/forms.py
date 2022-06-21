ff862ae23fb6a529f73a60b03c83a0ce13d3701f
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-21 12:20:06 +0300

    pt: add post form class

--- watch_app/forms.py
+++ watch_app/forms.py
@@ -2,27 +2,41 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+# app imports
+from watch_app.models import Post
+
 
 # user form
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email',]
+        fields = ['first_name', 'last_name', 'email', ]
 
         widgets = {
-            'first_name' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name..'}),
-            'last_name' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name..'}),
-            'email' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email...'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name..'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name..'}),
+            'email': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email...'}),
         }
-        
+
+
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
-        
+        fields = ['first_name', 'last_name', 'username',
+                  'email', 'password1', 'password2']
+
         widgets = {
-            'first_name' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name....'}),
-            'last_name' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name..'}),
-            'username' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'Username..'}),
-            'email' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email...'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name....'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name..'}),
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username..'}),
+            'email': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email...'}),
         }
+
+
+# post form
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'body', 'picture']
+        
+        
